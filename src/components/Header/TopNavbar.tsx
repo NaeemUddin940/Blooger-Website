9e2669513af7f534a5bfdad76423d166191c3109
MD. NAEEM UDDIN <mdnaeemuddin14@gmail.com>
2025-08-13 08:07:15 +0600

    Top Navbar added

---
+++ src/components/Header/TopNavbar.tsx
@@ -0,0 +1,45 @@
+import { Facebook, Instagram, Linkedin, Twitter, Youtube } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+export default function TopNavbar() {
+  return (
+    <section className="flex justify-center bg-accent-foreground text-white py-1">
+      <div className="flex justify-between w-5xl">
+        <div>
+          <ul className="flex gap-5 [&>li:hover]:text-violet-500">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/about-us">About Us</Link>
+            </li>
+            <li>
+              <Link href="/contact-us">Contact Us</Link>
+            </li>
+            <li>
+              <Link href="/privacy-policy">Privacy Policy</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="flex gap-5">
+          <Link href="#" className="hover:text-[#1877F2]">
+            <Facebook size={20}/>
+          </Link>
+          <Link href="#" className="hover:text-[#FF0000]">
+            <Youtube size={20}/>
+          </Link>
+          <Link href="#" className="hover:text-[#1DA1F2]">
+            <Twitter size={20}/>
+          </Link>
+          <Link href="#" className="hover:text-[#E4405F]">
+            <Instagram size={20}/>
+          </Link>
+          <Link href="#" className="hover:text-[#0A66C2]">
+            <Linkedin size={20}/>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+}
