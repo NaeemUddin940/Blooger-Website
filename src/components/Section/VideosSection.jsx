d5d79d8cb00cb785726de8b1a81c4a8b3b49d6c8
MD. NAEEM UDDIN <mdnaeemuddin14@gmail.com>
2025-08-14 23:57:47 +0600

    Version 4.1

--- src/components/Section/VideosSection.jsx
+++ src/components/Section/VideosSection.jsx
@@ -110,7 +110,7 @@ export const VideosSection = () => {
         </div>
 
         {/* Other videos grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-2 md:grid-cols-1 lg:grid-cols-3 gap-8">
           {otherVideos.map((video) => (
             <VideoCard key={video.id} video={video} />
           ))}
