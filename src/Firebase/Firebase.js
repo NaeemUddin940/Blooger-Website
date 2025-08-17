fcbab2ac1db12c9cbbde74033c697608d0f6c45b
MD. NAEEM UDDIN <mdnaeemuddin14@gmail.com>
2025-08-18 00:51:46 +0600

    changes imp

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -7,14 +7,14 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBwGvfoVBSkEXSDaJJy5fsjTwGy9NDKUFw",
-  authDomain: "blog-website-63d98.firebaseapp.com",
-  projectId: "blog-website-63d98",
-  storageBucket: "blog-website-63d98.firebasestorage.app",
-  messagingSenderId: "764996484901",
-  appId: "1:764996484901:web:8ae803954fd44f4aa864cc",
-  measurementId: "G-HDRZWV247E",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
