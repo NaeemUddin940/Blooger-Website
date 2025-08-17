99b5af58809c6c4f1bf3bf33b6a0a52bbb0d49a8
MD. NAEEM UDDIN <mdnaeemuddin14@gmail.com>
2025-08-18 00:56:07 +0600

    firebase secure

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -13,8 +13,8 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
